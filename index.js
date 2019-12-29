2e737116665bb542edeef4bb3a319d346757d96b
Robin Parisot <parisrob@hotmail.fr>
2019-12-29 23:36:41 +0000

    edit commandes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const auth = require('./auth.json');
+const {prefix, token} = require('./auth.json');
 
 var jsdom = require("jsdom");
 const {JSDOM} = jsdom;
@@ -12,59 +12,106 @@ var $ = jQuery = require('jquery')(window);
 
 
 client.once('ready', () => {
+    client.user.setStatus('Dispo : ' + prefix + 'help');
     console.log('Ready!');
+
+    client.user.setActivity('Runing : ' + prefix + 'help', {type: 'PLAYING'});
 });
 
 client.on('message', message => {
-    if (message.content === '!sorties') {
-        $.ajax({
-            url: 'https://api.warframestat.us/pc/sortie',
-            method: "GET",
-            crossOrigin: true,
-            success: function (data) {
-                message.channel.send({embed: getEmbedSortie(data)});
-            }
-        })
+    const cmd = message.content;
+    if (!cmd.startsWith(prefix)) return;
+    switch (cmd) {
+        case prefix + 'sorties':
+            sorties(message);
+            break;
+        case prefix + 'help':
+            help(message);
+            break;
+        default:
+            help(message);
+            break;
     }
 });
 
-client.login(auth.token);
+client.login(token);
+
+function help(message) {
+    var embed = {
+        color: 0x0099ff,
+        title: 'Liste des commandes',
+        fields: [{
+            name: prefix + 'sorties',
+            value: 'Liste les 3 sorties du jour'
+        }, {
+            name: prefix + 'help',
+            value: 'Affiche cet aide'
+        }],
+        timestamp: new Date(),
+    };
+    message.channel.send({embed: embed});
+}
+
+function sorties(message) {
+    $.ajax({
+        url: 'https://api.warframestat.us/pc/sortie',
+        method: "GET",
+        crossOrigin: true,
+        success: function (data) {
+            message.channel.send({embed: getEmbedSortie(data)});
+        }
+    })
+}
 
 
 function getEmbedSortie(data) {
-    const embed = {
+    var embed = {
         color: 0x0099ff,
         title: data.boss + ' en ' + data.faction,
-        //url: 'https://discord.js.org',
-        /*author: {
-            name: 'Some name',
-            icon_url: 'https://i.imgur.com/wSTFkRM.png',
-            url: 'https://discord.js.org',
-        },*/
-        //description: 'Some description here',
-        /*thumbnail: {
-            url: 'https://i.imgur.com/wSTFkRM.png',
-        },*/
         fields: [],
-        /*image: {
-            url: 'https://i.imgur.com/wSTFkRM.png',
-        },*/
         timestamp: new Date(data.expiry),
-        /*footer: {
-            text: 'Fin d\'événement dans ' + new Date(data.expiry),
-            //icon_url: 'https://i.imgur.com/wSTFkRM.png',
-        },*/
     };
     $.each(data.variants, function (key, value) {
         embed.fields.push({
             name: "Mission => " + value.missionType,
             value: "Modifier => " + value.modifier + "\n Lieu => " + value.node,
         });
-        /*embed.fields.push({
-            name: '\u200b',
-            value: '\u200b',
-        });*/
     });
 
     return embed;
 }
+
+/*
+var embed = {
+    color: 0x0099ff,
+    title: data.boss + ' en ' + data.faction,
+    url: 'https://discord.js.org',
+    author: {
+        name: 'Some name',
+        icon_url: 'https://i.imgur.com/wSTFkRM.png',
+        url: 'https://discord.js.org',
+    },
+    description: 'Some description here',
+    thumbnail: {
+        url: 'https://i.imgur.com/wSTFkRM.png',
+    },
+    fields: [
+        {
+            name: "Mission => " + value.missionType,
+            value: "Modifier => " + value.modifier + "\n Lieu => " + value.node,
+        },
+        {
+            name: '\u200b',
+            value: '\u200b',
+        }
+    ],
+    image: {
+        url: 'https://i.imgur.com/wSTFkRM.png',
+    },
+    timestamp: new Date(data.expiry),
+    footer: {
+        text: 'Fin d\'événement dans ' + new Date(data.expiry),
+        icon_url: 'https://i.imgur.com/wSTFkRM.png',
+    },
+};
+*/
