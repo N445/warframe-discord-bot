6066aef45bedb8e8dfd762f2d7cf0a50b8afde9e
Robin Parisot <parisrob@hotmail.fr>
2019-12-31 11:34:41 +0000

    add emoji

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const Emoji =  require("discord-emoji");
 const {prefix, token} = require('./auth.json');
 
 var jsdom = require("jsdom");
@@ -132,23 +133,7 @@ const helpFunction = function help(message, options) {
 }
 
 const testFunction = function (message, option) {
-    const exampleEmbed = new Discord.MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle('Some title')
-        .setURL('https://discord.js.org/')
-        .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-        .setDescription('Some description here')
-        .setThumbnail('https://i.imgur.com/wSTFkRM.png')
-        .addField('Regular field title', 'Some value here')
-        .addBlankField()
-        .addField('Inline field title', 'Some value here', true)
-        .addField('Inline field title', 'Some value here', true)
-        .addField('Inline field title', 'Some value here', true)
-        .setImage('https://i.imgur.com/wSTFkRM.png')
-        .setTimestamp()
-        .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-
-    message.channel.send(exampleEmbed);
+    message.channel.send(Emoji.nature.cat);
 }
 
 function getBaseEmbed(){
